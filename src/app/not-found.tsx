d4c1cbfd9642f4f399e73b4eb7fa9b0b1688d29f
Hossein Mahdian <108767623+hosseinmahdiian@users.noreply.github.com>
2025-12-06 23:07:54 +0330

    edit ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
 const NotFound = () => {
-  return <div className=" md:h-[calc(100%-144px)] h-[calc(100%-80px)] text-center flex items-center justify-center font-bold ">Not Found</div>;
+  return <div className=" bg-[#343434]  md:h-[calc(100%-144px)] h-[calc(100%-80px)] text-center flex items-center justify-center font-bold ">Not Found</div>;
 };
 
 export default NotFound;
